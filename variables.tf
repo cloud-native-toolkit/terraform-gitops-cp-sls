d9182cef9aded41292f6ceef6913106ffa1feb58
Ramya Raghuveera <ramragh1@in.ibm.com>
2021-12-23 12:41:44 +0530

    Updated variables.tf with new variables

--- variables.tf
+++ variables.tf
@@ -62,11 +62,51 @@ variable "git_credentials" {
   description = "The credentials for the gitops repo(s)"
 }
 
-variable "namespace" {
+variable "sls_namespace" {
   type        = string
   description = "The namespace where the application should be deployed"
 }
+variable "sls_key" {
+  type        = string
+  description = "ibm container entitlement key for sls"
 
+}
+
+variable "sls_storageClass" {
+  type        = string
+  description = "Name for sls file storage class"
+  default = "portworx-db2-rwx-sc"
+}
+
+variable "mongo_userid" {
+  type        = string
+  description = "mongodb admin userid, should stay admin in most cases"
+  default     = "admin"
+}
+
+variable "mongo_dbpass" {
+  type        = string
+  description = "mongodb password"
+
+}
+
+variable "mongo_namespace" {
+  type        = string
+  description = "namespace for mongo"
+  
+}
+
+variable "mongo_svcname" {
+  type        = string
+  description = "service name for mongo"
+  
+}
+
+variable "certmgr_namespace" {
+  type        = string
+  description = "Namespace where cert-manager is deployed"
+  default = "cert-manager"
+}
 variable "cluster_ingress_hostname" {
   type        = string
   description = "Ingress hostname of the IKS cluster."
@@ -95,4 +135,21 @@ variable "server_name" {
   type        = string
   description = "The name of the server"
   default     = "default"
+}
+variable "catalog" {
+  type        = string
+  description = "The catalog source that should be used to deploy the operator"
+  default     = "ibm-operator-catalog"
+}
+
+variable "catalog_namespace" {
+  type        = string
+  description = "The namespace where the catalog has been deployed"
+  default     = "openshift-marketplace"
+}
+
+variable "channel" {
+  type        = string
+  description = "The channel that should be used to deploy the operator"
+  default     = "3.x"
 }
