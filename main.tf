261d164a2a7f1d9c024c406013d55d932284e92d
Ramya Raghuveera <ramragh1@in.ibm.com>
2021-12-31 16:14:54 +0530

    changed modulename in main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name          = "my-module"
+  name          ="ibm-sls-operator"
   bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
   ingress_host  = "${local.name}-${var.sls_namespace}.${var.cluster_ingress_hostname}"
