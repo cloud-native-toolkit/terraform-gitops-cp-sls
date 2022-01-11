b2c1a3fbb0c153e293b87ecbbe4377397a43921b
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-01-11 20:31:10 +0530

    component name change in validate deploy

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -9,8 +9,9 @@ BRANCH="main"
 SERVER_NAME="default"
 TYPE="base"
 LAYER="2-services"
-
-COMPONENT_NAME="ibm-sls-operator"
+#INSTANCE_NAME="ibm-sls-operator-instance"
+#COMPONENT_NAMES="ibm-sls-operator-subscription,ibm-sls-operator-instance"
+COMPONENT_NAMES="ibm-sls-operator-subscription"
 
 mkdir -p .testrepo
 
@@ -19,7 +20,9 @@ git clone https://${GIT_TOKEN}@${GIT_REPO} .testrepo
 cd .testrepo || exit 1
 
 find . -name "*"
-
+#IFS=","
+#for COMPONENT_NAME  in $COMPONENT_NAMES;
+#do
 if [[ ! -f "argocd/${LAYER}/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml" ]]; then
   echo "ArgoCD config missing - argocd/${LAYER}/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml"
   exit 1
@@ -35,13 +38,13 @@ fi
 
 echo "Printing payload/${LAYER}/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
 cat "payload/${LAYER}namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
-
+done
 count=0
 until kubectl get namespace "${NAMESPACE}" 1> /dev/null 2> /dev/null || [[ $count -eq 20 ]]; do
   echo "Waiting for namespace: ${NAMESPACE}"
   count=$((count + 1))
   sleep 15
-done
+#done
 
 if [[ $count -eq 20 ]]; then
   echo "Timed out waiting for namespace: ${NAMESPACE}"
@@ -51,6 +54,7 @@ else
   sleep 30
 fi
 
+#DEPLOYMENT="${INSTANCE_NAME}-${BRANCH}"
 DEPLOYMENT="${COMPONENT_NAME}-${BRANCH}"
 count=0
 until kubectl get deployment "${DEPLOYMENT}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
