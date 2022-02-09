16ec2ce7b584b26aec9452a9e67e3d211e0e44a1
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-09 14:18:59 -0600

    Updates test variables
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-sls.tf
+++ test/stages/stage2-sls.tf
@@ -6,8 +6,6 @@ module "gitops_module" {
   cluster_config_file = module.dev_cluster.config_file_path
   server_name = module.gitops.server_name
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
-  cluster_type = module.dev_cluster.platform.type_code
-  tls_secret_name = module.dev_cluster.platform.tls_secret
   kubeseal_cert = module.gitops.sealed_secrets_cert
   catalog = module.cp_catalogs.catalog_ibmoperators
   namespace   = module.dev_namespace.name
