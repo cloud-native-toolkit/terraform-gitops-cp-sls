640bac029614fe63005bd91501a086d1525fa179
tcskill <tskill@us.ibm.com>
2022-03-17 14:50:49 -0500

    fixes entitlement mapping in test stage

--- test/stages/stage1-cp-catalogs.tf
+++ test/stages/stage1-cp-catalogs.tf
@@ -5,5 +5,6 @@ module "cp_catalogs" {
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
   kubeseal_cert = module.gitops.sealed_secrets_cert
-  entitlement_key = "test"
+  entitlement_key = var.cp_entitlement_key
+  
 }
