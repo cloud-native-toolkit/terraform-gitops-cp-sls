b39ed8c54d2d5eddec0c7c5811b04c8083b5bbf5
tcskill <tskill@us.ibm.com>
2022-04-29 09:18:47 -0500

    removes cert

--- test/stages/stage1-certmgr.tf
+++
@@ -1,7 +0,0 @@
-module "jetstack-cert" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-ocp-cert-manager.git"
-
-  gitops_config = module.gitops.gitops_config
-  git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-}
